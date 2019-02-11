12777ce0d85dee251fc61d9c430405a185fec9f0
Nexus-sama <31282797+trevon1344@users.noreply.github.com>
2019-02-11 07:55:48 -0600

    Tail animation choices

--- amm_ta.py
+++ amm_ta.py
@@ -4,6 +4,7 @@
 
 import struct
 import math
+import os
 
 
 def bsk_type():
@@ -23,57 +24,314 @@ def main():
     print("BSK name (with extension):")
     x = input("")
     f = open(x, "r+b")
-    amm = open("Files\AMM - Tail.bin", "r+b")
-    amm_copy = amm.read()
-    chunk = f.read(16)
-    offsets = []
-    counter = 0
+    print("Which tail animation would you like to use?")
+    print("SSJ4, Cell, Frieza, Cooler?(S/C/F/CO)")
+    tail_type = input("")
+    tail_type = tail_type.lower()
+    if tail_type == "s" or tail_type == "ssj4":
+        # Copying files needed
+        bin_copy = f.read()
+        amm = open("Files\Tail_SSJ4.bin", "r+b")
+        amm_copy = amm.read()
 
-    #Changes the size of the 3rd AMM in the AMB of the  MAIN BSK bin
-    while chunk != b"":
-        if chunk[0] == 0x23 and chunk[1] == 0x41 and chunk[2] == 0x4D and chunk[3] == 0x42 and chunk[4] == 0x20 and chunk[12] == 0x03:
-            offsets.append(((f.tell()-16)))
-            print(hex(f.tell()-16))
-            counter += 1
-        chunk = f.read(16)
-    for i in offsets:
-        f.seek(i+84)
-        f.write(b"\x50\xF1\x02\x00")
-    f.close()
+        # Setting up temp bins
+        tn1 = "Files\z.bin"     # Will hold edited bin
+        temp1 = open(tn1, "w+b")
+        temp1.write(bin_copy)
+        temp1.close()
+        temp1 = open(tn1, "r+b")
+        tn2 = "Files\z2.bin"    # Will hold tail AMM
+        temp2 = open(tn2, "w+b")
+        temp2.write(amm_copy)
+        temp2.close()
+        temp2 = open(tn2, "r+b")
 
-    #replaces old 3rd AMM with Goku's for the tail animation data
-    f = open(x, "r+b")
-    chunk = f.read(16)
-    offsets = []
-    counter = 0
-    while chunk != b"":
-        if chunk[0] == 0x01 and chunk[1] == 0x01 and chunk[2] == 0x00 and chunk[3] == 0x00 and chunk[4] == 0x20:
-            offsets.append(((f.tell() - 16)))
-            print(hex(f.tell() - 16))
-            counter += 1
-        chunk = f.read(16)
-    for i in offsets:
-        f.seek(i + 0)
-        f.write(amm_copy)
+        # Changes the size of the 3rd AMM in the AMB of the  MAIN BSK bin
+        temp1.seek(84)
+        temp1.write(b'\x50\xF1\x02\x00')
 
-    #closes bin
-    f.close()
-    f = open(x, "r+b")
-    chunk = f.read()
+        # Goes to 3rd AMM
+        temp1.seek(80)
+        offset = temp1.read(4)
+        print("bytes: " + str(offset))
+        offset = offset.hex()
+        print("hex(LE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        offset = struct.pack('<L', offset)
+        print("bytes(BE): " + str(offset))
+        offset = offset.hex()
+        print("hex(BE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(offset)
+        t_copy = temp2.read()
+        temp1.write(t_copy)
+
+        # Deletes extra bytes at the bottom of the bin
+        temp1.seek(84)
+        offset2 = temp1.read(4)
+        print("bytes: " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(LE): " + str(offset2))
+        offset2 = int(offset2, 16)
+        print("dec: " + str(offset2))
+        offset2 = struct.pack('<L', offset2)
+        print("bytes(BE): " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(BE): " + str(offset))
+        offset2 = int(offset2, 16) + offset
+        print("dec: " + str(offset2))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(0)
+        t_copy = temp1.read(offset2)
+        tn3 = "Files\z3.bin"
+        temp3 = open(tn3, "w+b")
+        temp3.write(t_copy)
+    if tail_type == "c" or tail_type == "cell":
+        # Copying files needed
+        bin_copy = f.read()
+        amm = open("Files\Tail_Cell.bin", "r+b")
+        amm_copy = amm.read()
+
+        # Setting up temp bins
+        tn1 = "Files\z.bin"  # Will hold edited bin
+        temp1 = open(tn1, "w+b")
+        temp1.write(bin_copy)
+        temp1.close()
+        temp1 = open(tn1, "r+b")
+        tn2 = "Files\z2.bin"  # Will hold tail AMM
+        temp2 = open(tn2, "w+b")
+        temp2.write(amm_copy)
+        temp2.close()
+        temp2 = open(tn2, "r+b")
+
+        # Changes the size of the 3rd AMM in the AMB of the  MAIN BSK bin
+        temp1.seek(84)
+        temp1.write(b'\xE4\xF2\x02\x00')
+
+        # Goes to 3rd AMM
+        temp1.seek(80)
+        offset = temp1.read(4)
+        print("bytes: " + str(offset))
+        offset = offset.hex()
+        print("hex(LE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        offset = struct.pack('<L', offset)
+        print("bytes(BE): " + str(offset))
+        offset = offset.hex()
+        print("hex(BE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(offset)
+        t_copy = temp2.read()
+        temp1.write(t_copy)
+
+        # Deletes extra bytes at the bottom of the bin
+        temp1.seek(84)
+        offset2 = temp1.read(4)
+        print("bytes: " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(LE): " + str(offset2))
+        offset2 = int(offset2, 16)
+        print("dec: " + str(offset2))
+        offset2 = struct.pack('<L', offset2)
+        print("bytes(BE): " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(BE): " + str(offset))
+        offset2 = int(offset2, 16)+offset
+        print("dec: " + str(offset2))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(0)
+        t_copy = temp1.read(offset2)
+        tn3 = "Files\z3.bin"
+        temp3 = open(tn3, "w+b")
+        temp3.write(t_copy)
+
+    if tail_type == "f" or tail_type == "frieza":
+        # Copying files needed
+        bin_copy = f.read()
+        amm = open("Files\Tail_Frieza.bin", "r+b")
+        amm_copy = amm.read()
+
+        # Setting up temp bins
+        tn1 = "Files\z.bin"  # Will hold edited bin
+        temp1 = open(tn1, "w+b")
+        temp1.write(bin_copy)
+        temp1.close()
+        temp1 = open(tn1, "r+b")
+        tn2 = "Files\z2.bin"  # Will hold tail AMM
+        temp2 = open(tn2, "w+b")
+        temp2.write(amm_copy)
+        temp2.close()
+        temp2 = open(tn2, "r+b")
+
+        # Changes the size of the 3rd AMM in the AMB of the  MAIN BSK bin
+        temp1.seek(84)
+        temp1.write(b'\x88\xF8\x02\x00')
+
+
+        # Goes to 3rd AMM
+        temp1.seek(80)
+        offset = temp1.read(4)
+        print("bytes: " + str(offset))
+        offset = offset.hex()
+        print("hex(LE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        offset = struct.pack('<L', offset)
+        print("bytes(BE): " + str(offset))
+        offset = offset.hex()
+        print("hex(BE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(offset)
+        t_copy = temp2.read()
+        temp1.write(t_copy)
+
+        # Deletes extra bytes at the bottom of the bin
+        temp1.seek(84)
+        offset2 = temp1.read(4)
+        print("bytes: " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(LE): " + str(offset2))
+        offset2 = int(offset2, 16)
+        print("dec: " + str(offset2))
+        offset2 = struct.pack('<L', offset2)
+        print("bytes(BE): " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(BE): " + str(offset))
+        offset2 = int(offset2, 16)+offset
+        print("dec: " + str(offset2))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(0)
+        t_copy = temp1.read(offset2)
+        tn3 = "Files\z3.bin"
+        temp3 = open(tn3, "w+b")
+        temp3.write(t_copy)
+
+    if tail_type == "co" or tail_type == "cooler":
+        # Copying files needed
+        bin_copy = f.read()
+        amm = open("Files\Tail_Cooler.bin", "r+b")
+        amm_copy = amm.read()
+
+        # Setting up temp bins
+        tn1 = "Files\z.bin"  # Will hold edited bin
+        temp1 = open(tn1, "w+b")
+        temp1.write(bin_copy)
+        temp1.close()
+        temp1 = open(tn1, "r+b")
+        tn2 = "Files\z2.bin"  # Will hold tail AMM
+        temp2 = open(tn2, "w+b")
+        temp2.write(amm_copy)
+        temp2.close()
+        temp2 = open(tn2, "r+b")
+
+        # Changes the size of the 3rd AMM in the AMB of the  MAIN BSK bin
+        temp1.seek(84)
+        temp1.write(b'\x20\xA0\x02\x00')
+
+        # Goes to 3rd AMM
+        temp1.seek(80)
+        offset = temp1.read(4)
+        print("bytes: " + str(offset))
+        offset = offset.hex()
+        print("hex(LE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        offset = struct.pack('<L', offset)
+        print("bytes(BE): " + str(offset))
+        offset = offset.hex()
+        print("hex(BE): " + str(offset))
+        offset = int(offset, 16)
+        print("dec: " + str(offset))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(offset)
+        t_copy = temp2.read()
+        temp1.write(t_copy)
+
+        # Deletes extra bytes at the bottom of the bin
+        temp1.seek(84)
+        offset2 = temp1.read(4)
+        print("bytes: " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(LE): " + str(offset2))
+        offset2 = int(offset2, 16)
+        print("dec: " + str(offset2))
+        offset2 = struct.pack('<L', offset2)
+        print("bytes(BE): " + str(offset2))
+        offset2 = offset2.hex()
+        print("hex(BE): " + str(offset))
+        offset2 = int(offset2, 16)+offset
+        print("dec: " + str(offset2))
+        print("")
+
+        # Paste over old AMM with new AMM
+        temp1.seek(0)
+        t_copy = temp1.read(offset2)
+        tn3 = "Files\z3.bin"
+        temp3 = open(tn3, "w+b")
+        temp3.write(t_copy)
 
     # replacing old bytes with new ones
+    temp3.close()
+    temp3 = open(tn3, "r+b")
+    t_copy = temp3.read()
     old_b = b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
     new_b = b'\xFF\xFF\xFF\xFF\x00\x00\x00\x00\xFF\xFF\xFF\xFF'
-    chunk = chunk.replace(old_b, new_b)
-    f.close
+    t_copy = t_copy.replace(old_b, new_b)
+    temp3.close
+    temp3 = open(tn3, "r+b")
+    temp3.write(t_copy)
 
-    f = open(x, "r+b")
-    f.seek(0)
-    f.write(chunk)
+
+    # Saves it to your file
+    temp3.close()
+    temp3 = open(tn3, "r+b")
+    t_copy = temp3.read(offset2)
+    f.close()
+    temp1.close()
+    temp2.close()
+    temp3.close()
+    os.remove(x)
+    f = open(x, "w+b")
+    f.write(t_copy)
+    f.close()
+    print("")
     print("Completed!")
 
-    # closes bin
-    f.close()
+    # deletes temp files
+    tn1 = "Files\z.bin"
+    temp1 = open(tn1, "r+b")
+    temp1.close()
+    tn2 = "Files\z2.bin"
+    temp2 = open(tn2, "r+b")
+    temp2.close()
+    tn3 = "Files\z3.bin"
+    temp3 = open(tn3, "r+b")
+    temp3.close()
+    os.remove(tn1)
+    os.remove(tn2)
+    os.remove(tn3)
 
 def special():
     # opens BSK
