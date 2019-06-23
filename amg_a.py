5955240ee691da21bf34f558150347f3124a47a7
Samuel <41597273+SamuelDBZMAAM@users.noreply.github.com>
2019-06-23 13:59:26 +0100

    Add files via upload
    updated amg_a

--- amg_a.py
+++ amg_a.py
@@ -8,145 +8,217 @@ print("With this you can only add one AMG at a time")
 
 import struct
 import math
-
-def start():
-    print("If you haven't given the model a new line on the AMO0")
-    print("You can add it here, press Enter to skip or type A then enter to add")
-    line_choice = input("")
-    line_choice = line_choice.lower()
-    if line_choice == "a":
-        import amo_a
-        amo_a.main()
-
-    else:
-        print("")
-        main()
         
 
 def main():
-    print("AMG Editor, Insert AMG file:")
-
-    #only will add the amg and will not edit any texture or shader values
-    #on the amg jsut yet, this will be later after the main code is done
-
+    #print("")
+    print("Insert AMO:")
     x = input("")
-    amg = open(x, "r+b")
-    amg.seek(28)
-    amg_length = amg.read(3)
-    amg.seek(0)
-    amg_copy = amg.read()
-   # print(amg_length)
-
-    print("Insert AMO file:")
-    x = input("")
+    x = x.replace("\"", "")
     f = open(x, "r+b")
-    chunk = f.read(16)
-    f.seek(0)
 
-    #grabing values like length of amo, amount of amgs, etc
+    print("Insert AMG to add:")
+    y = input("")
+    y = y.replace("\"", "")
+    amg = open(y, "r+b")
 
-    f.seek(24)#amount of amgs
-    am_amg = f.read(1)
-    am_amg = int(am_amg.hex(), 16)
-    #print(am_amg)
-    
-    #f.seek(0)
-    #f.seek(23)
-    #f.write(am_amg)
+    #open and write amg to amo
+    full_amg = amg.read()
+    amg_pos = f.seek(0,2)
+    f.write(full_amg)
 
-    f.seek(0)
-
-    f.seek(36)#length of amo
-    len_amo = f.read(3)
-    len_amo = int(len_amo.hex(), 16)
-
-    f.seek(0)
-
-    amo_head = 48#finding the next empty space
-    next_space = (amo_head + (am_amg * 4)) 
-    f.seek(next_space)
-    #print(f.tell())
-
-    f.seek(0,2)#goes to end of file, pastes the amg, gets it's location
-    amg_loc = f.tell()
-    amg_ax_loc = amg_loc + 32
-    #print(f.tell())
-    #print("locbelow")
-    #print(amg_ax_loc)
-    f.write(amg_copy)
-    f.seek(0)
-
-    f.seek(next_space)#seeks to the next space and writes the offset
-    amg_loc = struct.pack("<I", amg_loc)
-    
-    f.write(amg_loc)
-
-    f.seek(0)
-
-    #asks what axis to replace in decimal, user must know what to replace
-    print("start counting from _body axis...")
-    ax_to_rep = int(input("Number of axis to replace (dec.): "))
-    ax_to_rep -= 1
-    #print(ax_to_rep)
-    ax_to_rep = int(ax_to_rep)
-    #print(ax_to_rep)
-    ax_to_rep = struct.pack("<I", ax_to_rep)
-    #print(ax_to_rep)
-    #print(ax_to_rep[0])
-
-    #ax_to_rep_line = (ax_to_rep + b"\x00\x00\x00")
-    #print(ax_to_rep_line)
-
-    atp_offset = []
-
-    f.seek(0)
-
-    #searches for all axis points with the id
-    while chunk != b"":
-        if chunk[4] == ax_to_rep[0] and chunk[5] == ax_to_rep[1] and chunk[6] == ax_to_rep[2] and chunk[7] == ax_to_rep[3]:
-            atp_offset.append(((f.tell() - 16)))
-            print(f.tell()-16)
-        chunk = f.read(16)
-
-    #changes amg_ax_loc to bytes to write
-    amg_ax_loc = struct.pack("<I", amg_ax_loc)
-
-    #certain ids will be found throughout the file, so it will be cut out with
-    #the amount of times it should be there
-    #print(len(atp_offset))
-    f.seek(0)
-    f.seek(32)
-    am_axis = f.read(1)
-    #print(am_axis)
-    am_axis = int(am_axis.hex(), 16)
-    #print(am_axis)
-    atp_offset = atp_offset[0:am_axis]
-    #print(len(atp_offset))
-
-    
-    #doesn't change id since it doesn't matter, only offset is changed
-    for i in atp_offset:
-        f.seek(i+8)
-        f.write(amg_ax_loc)
-
-
-    #misc actions, editing the length of amo, editing the amg amount
+    #writes necessary data to amo
     f.seek(0)
     f.seek(24)
-    am_amg += 1
-    am_amg = struct.pack("<I", am_amg)
-    f.write(am_amg)
+    hti = f.read(4)
+    amg_amnt = hex_to_int(hti)
+    ith = (amg_amnt + 1)
+    new_amnt = int_to_hex(ith)
     f.seek(0)
-    f.seek(0,2)
-    len_amo = f.tell()
-    len_amo = struct.pack("<I", len_amo)
+    f.seek(24)
+    f.write(new_amnt)
+    
+    amo_head = 48
+    next_space = (amo_head + (amg_amnt * 4))
+    f.seek(0)
+    f.seek(next_space)
+    ith = amg_pos
+    ith2 = amg_pos + 32
+    new_loc = int_to_hex(ith)
+    amg_axis_loc = int_to_hex(ith2)
+    
+    f.write(new_loc)
+
+    f.seek(0)
+    amo_len = f.seek(0,2)
+    ith = amo_len
+    amo_len = int_to_hex(ith)
     f.seek(0)
     f.seek(36)
-    f.write(len_amo)
+    f.write(amo_len)
+
+
+    #locates each axis name and line
+    f_axis_names = []
+    f_axis_line_pos = []
+
+    #--gathering names
+
+    #-gathering locations and sizes
+
+    #first amg location
+    f.seek(0)
+    f.seek(48)
+    hti = f.read(4)
+    f_amg_loc = hex_to_int(hti)
+
+    #amount of axis
+    f.seek(0)
+    f.seek(16)
+    hti = f.read(4)
+    f_name_amnt = hex_to_int(hti)
+
+    f_amnt = f_name_amnt
+    f_name_amnt = (f_name_amnt * 32)
+
+
+    #axis line size
+    f.seek(0)
+    f.seek(32)
+    hti = f.read(4)
+    f_ax_li_len = hex_to_int(hti)
+
+
+    #axis line locations
+    f.seek(0)
+    f.seek(20)
+    hti = f.read(4)
+    f_bl_loc = hex_to_int(hti)
+
+
+    #first amg length
+    f.seek(0)
+    f.seek(f_amg_loc + 28)
+    hti = f.read(4)
+    f_amg_len = hex_to_int(hti)
+
+
+    #gathering names of axis
+    for i in range(((f_amnt))):
+        f.seek(0)
+        f.seek((f_amg_loc + (i*32) + f_amg_len))
+        f_axis_names.append(f.read(32))
+
+    #gathering axis line pos
+    for i in range(((f_amnt))):
+        f.seek(0)
+        f.seek(f_bl_loc + (f_name_amnt) + (i*f_ax_li_len*16))
+        f_axis_line_pos.append(f.tell())
+
+
+
+
+    f_new_names = []
+
+    #-Writing New Values
+
+    #fixes names
+    for i in range(int(len(f_axis_names))):
+        #print("")
+        new_value = f_axis_names[i]
+        #print(new_value)
+        new_value = new_value.replace(b"\x00", b"\x2e")
+        #print(new_value)
+        new_value = str(bytes(new_value))
+        #print(new_value)
+        new_value = new_value.replace(".", "")
+        #print(new_value)
+        new_value = new_value.replace("b'", "")
+        #print(new_value)
+        new_value = new_value.replace("'", "")
+        #print(new_value)
+        new_value = new_value.replace("X", "")
+        #print(new_value)
+        new_value = new_value.replace("_", "")
+        #print(new_value)
+        new_value = new_value[3:]
+        #print(new_value)
+        f_new_names.append(new_value)
+
+
+    #new text file for pastingin new data
+    text_file = open("b3_axis_locs.txt", "w+")
+
+
+
+    cancel = False
+    while cancel != True:
+        print(f_new_names)
+        print("Choose an axis from the add-on model")
+        f_choice = input("")
+        f_choice = f_choice.upper()
+
+        while f_choice not in f_new_names:
+            print("Not a valid axis, try again.")
+            f_choice = input("")
+            f_choice = f_choice.upper()
+
+        f_cho_pos = int(f_new_names.index(f_choice))
+
+
+        
+        f.seek(0)
+        for i in range(f_ax_li_len):
+            f.seek(f_axis_line_pos[f_cho_pos]+8+(i*16))
+            f.write(amg_axis_loc)
+        
+        axi_loc_name = amg_axis_loc
+        axi_loc_name = hex_to_int(axi_loc_name)
+        axi_loc_name = hex(axi_loc_name)
+        axi_loc_name = axi_loc_name.replace("0x", "")
+
+        
+        #writes data to text file for people who want to further use
+        text_file.write(str(f_choice) + " = " + str(axi_loc_name) + "\n")
+
+        print("Added!")
+        cancel = True
     
 
+    text_file.close()
+
+
+
+
+
+
+
+    
+
+    #closing files
     f.close()
     amg.close()
+    
+
+
+def hex_to_int(hti):
+    # Converts hex offsets to integer format
+    hti = hti.hex()
+    hti = int(hti, 16)
+    hti = struct.pack('<L', hti)
+    hti = hti.hex()
+    hti = int(hti, 16)
+    #print("DEBUG:HTI - " + str(hti))
+    return hti
+
+
+def int_to_hex(ith):
+    # Opposite of hex_to_int
+    ith = struct.pack('<L', ith)
+    #print("DEBUG:ITH - " + str(ith))
+    return ith
+
+
 
 def again():
     yn = input("Load another? (Y/N)")
@@ -157,7 +229,6 @@ def again():
         again()
     else:
         kill = input("press enter to close")
-
-start()
+main()
 again()
 exit()
